2b0c593e674ece4f34bd8c1e0b1255c997d68bdc
Skaranasios <stathis.karanasios@outlook.com>
2022-02-11 22:05:20 +0200

    json objects filling  templates wtih replace method

--- index.js
+++ index.js
@@ -19,17 +19,50 @@ const url =require('url');
 
 //Server
 
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const replaceTemplate = (temp,product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g , 'not-organic');
+
+    return output;
+    
+}
+
+
+//array.map
+//el is what holds data
 
 
 const server=http.createServer((req,res) => {
     console.log(req.url);
     //console.log(req);
     const pathName = req.url;
-    if (pathName=== "/overview")  {
-        res.end("This is overview");
+    if (pathName=== "/overview" ||  pathName === "/")  {
+        res.writeHead(200,{
+            'Content-type': 'text/html'
+        });
+        //loop over dataObj and with each itteration will replace tempalte-product/card
+        //in arrow fnction not neccesary to write return
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+        //console.log(cardsHtml);
+        const output = tempOverview.replace('{%PRODUCTS_CARDS%}', cardsHtml);
+        //console.log(cardsHtml);
+        //read template overview
+        res.end(output);
     }
     else if (pathName === "/api"){
         
@@ -37,8 +70,7 @@ const server=http.createServer((req,res) => {
                 'Content-type': 'application/json'});
             res.end(data);
     }
-    else if (pathName=== "/html")  {
-        res.end("This is html");}
+    
     else {
         res.writeHead(404,{
             'Content-type':'text/html',
